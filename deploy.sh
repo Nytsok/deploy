f22e5c511a9eeab470802aa4b52b47cf695087a3
Nytsok <64953453+Nytsok@users.noreply.github.com>
2025-12-23 14:58:36 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git clone https://github.com/fsoc106/Exegold /home/user/Exegold
 cd /home/user/Exegold
 python3 -m venv --system-site-packages ./venv
 source ./venv/bin/activate
-python3 -m pip install --user --requirement "requirements.txt"
+python3 -m pip install --requirement "requirements.txt"
 deactivate
 sudo ln -s "$(pwd)/Exegold/exegol.py" "/usr/local/bin/exegol"
 cd /home/user/mydeploy
